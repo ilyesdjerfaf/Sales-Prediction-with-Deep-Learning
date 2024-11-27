459ccff17cdae6821592380f25f5d863feadc2ed
ilyesdjerfaf <idjerfaf@gmail.com>
2024-11-27 17:42:17 +0100

    Flake8 the initial tests

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 import unittest
 
+
 class TestSalesPrediction(unittest.TestCase):
     def test_placeholder(self):
         # TODO - Implement test
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
